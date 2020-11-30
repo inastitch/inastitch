dbaee57f2eb4558161bd8fea13682e838dbd0c20
vejipe <>
2020-11-30 11:24:44 +0100

    Add face detection in tools/calibration/inastitch_cal_detailed

--- tools/calibration/inastitch_cal_detailed.cpp
+++ tools/calibration/inastitch_cal_detailed.cpp
@@ -34,6 +34,10 @@ namespace po = boost::program_options;
 #include "opencv2/stitching/detail/warpers.hpp"
 #include "opencv2/stitching/warpers.hpp"
 
+// Dlib
+#include <dlib/opencv.h>
+#include <dlib/image_processing/frontal_face_detector.h>
+
 // Std includes:
 #include <iostream>
 #include <fstream>
@@ -54,12 +58,15 @@ int main(int argc, char** argv)
 {
     std::vector<std::string> inputImagePaths;
 	float ratio, reprojThresh;
-    float rotationAngle;
+    float rotationAngleDeg = 0.0;
+    float maxWarpAngleDeg;
 
     uint16_t cropWidth;
     uint16_t cropHeight;
 
     std::string outputPath;
+
+    bool isAngleOverwritten = false;
 
     std::cout << "Calibration tool "
               << inastitch::version::GIT_COMMIT_TAG
@@ -71,13 +78,15 @@ int main(int argc, char** argv)
         desc.add_options()
             ("input-image", po::value<std::vector<std::string>>(&inputImagePaths),
              "Input images (a list of space-separated image paths)")
+            ("angle,a", po::value<float>(&rotationAngleDeg),
+             "Force rotation angle (disable face detection), in degrees")
 
+            ("max-warp-angle", po::value<float>(&maxWarpAngleDeg)->default_value(45.0),
+             "Max angle for warping input, in degrees")
             ("ratio", po::value<float>(&ratio)->default_value(0.75),
 	         "Ratio")
             ("reproj-thresh", po::value<float>(&reprojThresh)->default_value(4.0),
 	         "Reprojection threshold")
-            ("angle,a", po::value<float>(&rotationAngle)->default_value(0.0),
-             "Rotation angle")
 
             ("crop-width", po::value<uint16_t>(&cropWidth)->default_value(640),
              "Crop width")
@@ -107,19 +116,23 @@ int main(int argc, char** argv)
             std::cout << "No image to process" << std::endl;
             return 0;
         }
-	}
 
-    const auto inputImageCount = inputImagePaths.size();
-    std::vector<cv::Mat> inputImages(inputImageCount);
+        if(vm.count("angle")) {
+            isAngleOverwritten = true;
+        }
+	}
+    const float maxWarpAngleRad = maxWarpAngleDeg * 3.141592653589793 / 180.0;
 
     // Part0: Load input images
+    const auto inputImageCount = inputImagePaths.size();
+    std::vector<cv::Mat> inputImages(inputImageCount);
     for(uint32_t pathIdx=0; pathIdx<inputImageCount; pathIdx++)
     {
         inputImages.at(pathIdx) = cv::imread(inputImagePaths.at(pathIdx));
     }
     std::cout << "Loaded " << inputImages.size() << " images" << std::endl;
 
-    // Part1: Find features
+    // Part1: Find image features
     std::vector<cv::detail::ImageFeatures> features(inputImageCount);
     {
         auto finder = cv::SIFT::create();
@@ -134,7 +147,7 @@ int main(int argc, char** argv)
         }
     }
 
-    // Part2: Pairwise matching
+    // Part2: Pairwise matching between features in different images
     std::vector<cv::detail::MatchesInfo> pairwiseMatches(inputImageCount);
     {
         auto matcher = cv::detail::BestOf2NearestMatcher(false /* tryCuda */, ratio);
@@ -148,6 +161,7 @@ int main(int argc, char** argv)
     }
 
     // Part3: Homography estimation
+    // Find camera K and R matrices
     std::vector<cv::detail::CameraParams> cameraParams(inputImageCount);
     {
         auto estimator = cv::detail::HomographyBasedEstimator();
@@ -183,16 +197,102 @@ int main(int argc, char** argv)
         adjuster(features, pairwiseMatches, cameraParams);
     }
 
-    // Apply additional rotation
+    // Part4: Rotation matrix decomposition
+    // From rotation matrix to Euler angles
+    std::vector<float> yAngles(inputImageCount);
     {
-        const auto angleRad = rotationAngle * 3.141592653589793 / 180.0;
+        for(uint32_t imgIdx=0; imgIdx<inputImageCount; imgIdx++)
+        {
+            cv::Mat rotationVect;
+            // See: "cv::Rodrigues" in https://docs.opencv.org/4.4.0/d9/d0c/group__calib3d.html
+            cv::Rodrigues(cameraParams.at(imgIdx).R, rotationVect);
+
+            // transform to degree for printing
+            const auto rotationVectDeg = rotationVect * (180.0/3.141592653589793238463);
+            std::cout << "Image" << imgIdx << ": RotationVect=" << rotationVectDeg << std::endl;
+
+            // keep rotation on Y-axis only
+            yAngles.at(imgIdx) = rotationVect.at<float>(1);
+        }
+    }
+
+    float additionalRotationAngleRad;
+    if(isAngleOverwritten)
+    {
+        additionalRotationAngleRad = rotationAngleDeg * 3.141592653589793 / 180.0;
+    }
+    else
+    {
+        // Part5: Detect faces
+        uint32_t faceImgIdx;
+        cv::Point2f faceCenterPoint;
+        {
+            dlib::frontal_face_detector detector = dlib::get_frontal_face_detector();
+            for(uint32_t imgIdx=0; imgIdx<inputImageCount; imgIdx++)
+            {
+                // Convert OpenCV image to Dlib image
+                // See: http://dlib.net/imaging.html#cv_image
+                dlib::cv_image<dlib::bgr_pixel> dlibImage(inputImages.at(imgIdx));
+
+                const std::vector<dlib::rectangle> faceRects = detector(dlibImage);
+                for(const auto &rect : faceRects)
+                {
+                    std::cout << "Image" << imgIdx << ": FaceRect " << rect << std::endl;
+
+                    faceImgIdx = imgIdx;
+                    // center point
+                    const auto faceCenterX = rect.tl_corner().x() + (rect.width()/2);
+                    const auto faceCenterY = rect.tl_corner().y() + (rect.height()/2);
+
+                    faceCenterPoint = cv::Point2f(faceCenterX, faceCenterY);
+                }
+            }
+        }
+        // TODO: this code only selects the last face
+
+        // Part6: Find face angle
+        float faceAngleRad;
+        {
+            auto warperCreator = cv::PlaneWarper();
+            auto warper = warperCreator.create(cameraParams.at(0).focal);
+
+            // Image center point
+            const auto imageSize = inputImages.at(faceImgIdx).size();
+            cv::Point2f imageCenterPoint = cv::Point2f(imageSize.width/2.0f, imageSize.height/2.0f);
+
+            // warp the face center point
+            // Make float K
+            cv::Mat_<float> Kf;
+            cameraParams.at(faceImgIdx).K().convertTo(Kf, CV_32F);
+            const auto warpImageCenterPoint = warper->warpPoint(
+                        imageCenterPoint, Kf, cameraParams.at(faceImgIdx).R);
+            const auto warpFaceCenterPoint  = warper->warpPoint(
+                        faceCenterPoint, Kf, cameraParams.at(faceImgIdx).R);
+
+            // face angle
+            const auto xRatio = warpImageCenterPoint.x / warpFaceCenterPoint.x;
+            faceAngleRad = yAngles.at(faceImgIdx) * xRatio;
+
+            const auto faceAngleDeg = faceAngleRad * (180.0/3.141592653589793238463);
+            std::cout << "Face ratio: " << xRatio << " angle: " << faceAngleDeg << std::endl;
+        }
+        additionalRotationAngleRad = -faceAngleRad;
+    }
+
+
+    // Part7: Apply additional rotation angle
+    {
+        const auto rotationAngleRad = additionalRotationAngleRad;
+
+        // make rotation matrix
         cv::Mat R_y = (cv::Mat_<float>(3,3) <<
-           std::cos(angleRad),  0.0, std::sin(angleRad),
+           std::cos(rotationAngleRad),  0.0, std::sin(rotationAngleRad),
            0.0,                 1.0, 0.0,
-           -std::sin(angleRad), 0.0, std::cos(angleRad)
+           -std::sin(rotationAngleRad), 0.0, std::cos(rotationAngleRad)
         );
         // TODO: replace with cv::getRotationMatrix3D (?)
 
+        // apply rotation matrix to each camera
         for(uint32_t imgIdx=0; imgIdx<inputImageCount; imgIdx++)
         {
             auto R_rot = R_y * cameraParams.at(imgIdx).R;
@@ -200,9 +300,9 @@ int main(int argc, char** argv)
         }
     }
 
-    // Part4: Warp images
-    std::vector<cv::Mat> warpedImages(inputImageCount);
-    std::vector<cv::Rect> warpedImageRects(inputImageCount);
+    // Part8: Warp images
+    std::vector<cv::Mat> warpedImages;
+    std::vector<cv::Rect> warpedImageRects;
     cv::Rect warpedCropRect;
     {
         auto warperCreator = cv::PlaneWarper();
@@ -214,21 +314,27 @@ int main(int argc, char** argv)
 
         for(uint32_t imgIdx=0; imgIdx<inputImageCount; imgIdx++)
         {
-            // Make float K
-            cv::Mat_<float> Kf;
-            cameraParams.at(imgIdx).K().convertTo(Kf, CV_32F);
+            if(std::abs(yAngles.at(imgIdx)+additionalRotationAngleRad) < maxWarpAngleRad)
+            {
+                // Make float K
+                cv::Mat_<float> Kf;
+                cameraParams.at(imgIdx).K().convertTo(Kf, CV_32F);
 
-            warper->warp(inputImages.at(imgIdx), Kf, cameraParams.at(imgIdx).R,
-                         cv::INTER_LINEAR, cv::BORDER_CONSTANT,
-                         warpedImages.at(imgIdx));
+                // warp image
+                auto &warpedImage = warpedImages.emplace_back();
+                warper->warp(inputImages.at(imgIdx), Kf, cameraParams.at(imgIdx).R,
+                             cv::INTER_LINEAR, cv::BORDER_CONSTANT,
+                             warpedImage);
 
-            // warp corners
-            warpedImageRects.at(imgIdx) = warper->warpRoi(
-                inputImages.at(imgIdx).size(), Kf, cameraParams.at(imgIdx).R
-            );
+                // warp corners
+                auto &warpedImageRect = warpedImageRects.emplace_back();
+                warpedImageRect = warper->warpRoi(
+                    inputImages.at(imgIdx).size(), Kf, cameraParams.at(imgIdx).R
+                );
 
-            std::cout << "Image" << imgIdx << ": ROI " << warpedImageRects.at(imgIdx) << std::endl;
-            //cv::imwrite("warped" + std::to_string(imgIdx) + ".jpg", warpedImages.at(imgIdx));
+                std::cout << "Image" << imgIdx << ": ROI " << warpedImageRect << std::endl;
+                //cv::imwrite("warped" + std::to_string(imgIdx) + ".jpg", warpedImage);
+            }
         }
 
         {
@@ -245,8 +351,9 @@ int main(int argc, char** argv)
             std::cout << "WarpedCropRect: " << warpedCropRect << std::endl;
         }
     }
+    const auto warpedImageCount = warpedImages.size();
 
-    // Part5: Make panorama and crop
+    // Part9: Make panorama and crop
     {
         // Find min and max wrapped pixel coordinates
         int32_t minX = std::numeric_limits<int32_t>::max();
@@ -263,7 +370,7 @@ int main(int argc, char** argv)
             maxY = std::max(maxY, roi.y + roi.height);
         };
 
-        for(uint32_t imgIdx=0; imgIdx<inputImageCount; imgIdx++)
+        for(uint32_t imgIdx=0; imgIdx<warpedImageCount; imgIdx++)
         {
             const auto &roi = warpedImageRects.at(imgIdx);
 
@@ -286,7 +393,7 @@ int main(int argc, char** argv)
         {
             pano.setTo(0);
 
-            for(uint32_t imgIdx=0; imgIdx<inputImageCount; imgIdx++)
+            for(uint32_t imgIdx=0; imgIdx<warpedImageCount; imgIdx++)
             {
                 const auto &roi = warpedImageRects.at(imgIdx);
 
@@ -296,8 +403,7 @@ int main(int argc, char** argv)
             cv::imwrite("full_" + outputPath, pano);
         }
         // Note: depending on the additional rotation angle, the panoramic result may be very large.
-        // Two suggestions:
-        // - use spherical transformation instead of planar
+        // Solution:
         // - if only the cropped result is needed, it is not required to warp all inputs, espcially
         //   the ones with angles resulting in very strong perspective.
 
@@ -310,22 +416,6 @@ int main(int argc, char** argv)
 
             cv::Mat crop(pano, warpedCropRectWithOffset);
             cv::imwrite(outputPath, crop);
-        }
-    }
-
-    // Additional information
-    // Rotation matrix decomposition
-    {
-        for(uint32_t imgIdx=0; imgIdx<inputImageCount; imgIdx++)
-        {
-            cv::Mat rotationVect;
-            // See: "cv::Rodrigues" in https://docs.opencv.org/4.4.0/d9/d0c/group__calib3d.html
-            cv::Rodrigues(cameraParams.at(imgIdx).R, rotationVect);
-
-            // transform to degree
-            const auto rotationVectDeg = rotationVect * (180.0/3.141592653589793238463);
-
-            std::cout << "Image" << imgIdx << ": RotationVect=" << rotationVectDeg << std::endl;
         }
     }
     
